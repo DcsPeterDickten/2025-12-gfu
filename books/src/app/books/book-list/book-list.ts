cd39f2fca68c957e4f1175812b2065990aeaae60
Peter Dickten <peter.dickten@dcs-fuerth.de>
2025-12-01 15:33:19 +0100

    WIP

--- books/src/app/books/book-list/book-list.ts
+++ books/src/app/books/book-list/book-list.ts
@@ -1,11 +1,27 @@
+import { JsonPipe } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'books-book-list',
-  imports: [],
+  selector: 'books-list',
+  imports: [JsonPipe],
   templateUrl: './book-list.html',
   styleUrl: './book-list.css',
 })
 export class BookList {
+   books = [
+    {
+      isbn: '12345',
+      title: 'Angular 19',
+      price: 12.99
+    },{
+      isbn: '12346',
+      title: 'Angular 20',
+      price: 14.99
 
+    },{
+      isbn: '12347',
+      title: 'Angular 21',
+      price: 19.99
+    }
+   ];
 }
