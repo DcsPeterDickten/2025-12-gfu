53eef28a87171bdb01b4ccb9a9535490974ab336
Peter Dickten <peter.dickten@dcs-fuerth.de>
2025-12-02 09:24:15 +0100

    WIP

--- books/src/app/books/book-list/book-list.ts
+++ books/src/app/books/book-list/book-list.ts
@@ -7,24 +7,35 @@ import { Component } from '@angular/core';
   styleUrl: './book-list.css',
 })
 export class BookList {
-   books = [
+
+  books = [
     {
       isbn: '12345',
       title: 'Angular 19',
       price: 13.49,
       coverUrl: 'https://m.media-amazon.com/images/I/71Wv+d6oP6L._SY342_.jpg'
 
-    },{
+    }, {
       isbn: '12346',
       title: 'Angular 20',
       price: 14.99,
       coverUrl: 'https://m.media-amazon.com/images/I/71Wv+d6oP6L._SY342_.jpg'
 
-    },{
+    }, {
       isbn: '12347',
       title: 'Angular 21',
       price: 19.99,
       coverUrl: 'https://m.media-amazon.com/images/I/71Wv+d6oP6L._SY342_.jpg'
     }
-   ];
+  ];
+
+  coverIsVisible: boolean = true;
+
+  // ---- 
+
+  toggleCover() {
+    this.coverIsVisible = !this.coverIsVisible;
+    console.log('this.coverIsVisible', this.coverIsVisible);
+  }
+
 }
