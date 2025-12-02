301ee4fbbe8cc2e5035181adf17183c6f3ed2199
Peter Dickten <peter.dickten@dcs-fuerth.de>
2025-12-02 10:49:38 +0100

    WIP

---
+++ books/src/app/shared/calculator/calculator.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'calculator',
+  imports: [FormsModule],
+  templateUrl: './calculator.html',
+  styleUrl: './calculator.css',
+})
+export class Calculator {
+
+  x = 0;
+  y = 0;
+  result = 0;
+
+  add() {
+    this.result = this.x + this.y;
+  }
+
+  subtract() {
+    this.result = this.x - this.y;
+  }
+
+  clear() {
+    this.x = 0;
+    this.y = 0;
+    this.result = 0;
+  }
+}
