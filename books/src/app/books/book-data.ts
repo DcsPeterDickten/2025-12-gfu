703970c5d37c1bb2c24fbd5b63fd3b188606026b
Peter Dickten <peter.dickten@dcs-fuerth.de>
2025-12-03 10:04:22 +0100

    WIP

--- books/src/app/books/book-data.ts
+++ books/src/app/books/book-data.ts
@@ -1,42 +1,33 @@
 import { Injectable } from '@angular/core';
 import { Book } from './book';
 import { environment } from '../../environments/environment.development';
+import { HttpClient } from '@angular/common/http';
+import { firstValueFrom } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class BookData {
 
-  // getBooks(): Book[] {
-  //   return [
-  //     {
-  //       isbn: '12345',
-  //       title: 'Angular 19',
-  //       price: 13.0,
-  //       coverUrl: 'https://m.media-amazon.com/images/I/71Wv+d6oP6L._SY342_.jpg',
-  //       rating: 3.7
-  //     }, {
-  //       isbn: '12346',
-  //       title: 'Angular 20',
-  //       price: 14.99,
-  //       coverUrl: 'https://m.media-amazon.com/images/I/71Wv+d6oP6L._SY342_.jpg',
-  //       rating: 4.1
-  //     }, {
-  //       isbn: '12347',
-  //       title: 'Angular 21',
-  //       price: 20.99,
-  //       coverUrl: 'https://m.media-amazon.com/images/I/71Wv+d6oP6L._SY342_.jpg',
-  //       rating: 4.8
-  //     }
-  //   ]
-  // }
+  constructor(private httpClient: HttpClient) {
+  }
 
   async getBooks(): Promise<Book[]> {
     const URL = environment.SERVER_URL + '/books';
-    const response = await fetch(URL);
+    const response = await fetch(URL, { method: 'GET' });
     const books = await response.json() as Book[];
     return books;
   }
+
+
+  async getBookWithHttpClient(): Promise<Book[]> {
+    const URL = environment.SERVER_URL + '/books';
+    const observableBooks = this.httpClient.get<Book[]>(URL);
+    const promiseBooks = firstValueFrom(observableBooks);
+    return promiseBooks;
+  }
+
+
 
   getBook(isbn: string) {
     const URL = environment.SERVER_URL + '/books/' + isbn;
