cd849f1d63b49ccdc9bab77a5b6fd5f993d69af0
Peter Dickten <peter.dickten@dcs-fuerth.de>
2025-12-03 14:52:14 +0100

    WIP

--- books/src/app/books/welcome/welcome.ts
+++ books/src/app/books/welcome/welcome.ts
@@ -1,13 +1,13 @@
 import { AsyncPipe } from '@angular/common';
-import { Component } from '@angular/core';
-import { BehaviorSubject, filter, from, map, of, Subject, timer } from 'rxjs';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { BehaviorSubject, filter, from, map, of, ReplaySubject, Subject, Subscription, timer } from 'rxjs';
 
 @Component({
   imports: [AsyncPipe],
   templateUrl: './welcome.html',
   styleUrl: './welcome.css',
 })
-export class Welcome {
+export class Welcome implements OnInit, OnDestroy {
 
   // obs1$ = of(1, 2, 3);
   // obs2$ = from([4, 5, 6]);
@@ -17,7 +17,9 @@ export class Welcome {
   // );
   // obs5$ = this.obs4$.pipe(filter(y => y % 100 === 0));
 
-  mySubject$ = new BehaviorSubject<string>('Start');
+  // mySubject$ = new ReplaySubject<string>(10);
+
+  mySubscriptions$: Subscription[] = [];
 
   constructor() {
     // this.obs3$.subscribe((data) => console.log('obs3$', data));
@@ -26,11 +28,20 @@ export class Welcome {
     // // 5000 Sekunden Pause
     // obs6$.subscribe(console.log)
 
-    this.mySubject$.subscribe((neueInfo) => console.log('subject sagt - early', neueInfo));
-    this.mySubject$.next('Hallo!');
-    this.mySubject$.next('Welt!');
-    this.mySubject$.subscribe((neueInfo) => console.log('subject sagt - late', neueInfo));
+    // this.mySubject$.subscribe((neueInfo) => console.log('subject sagt - early', neueInfo));
+    // this.mySubject$.next('Hallo!');
+    // this.mySubject$.next('Welt!');
+    // this.mySubject$.subscribe((neueInfo) => console.log('subject sagt - late', neueInfo));
   }
+
+  ngOnInit() {
+    this.mySubscriptions$.push(timer(0, 500).subscribe(console.log));
+  }
+
+  ngOnDestroy(): void {
+    this.mySubscriptions$.forEach(s => s?.unsubscribe());
+  }
+
 
 
 }
